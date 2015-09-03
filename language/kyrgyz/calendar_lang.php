9614ef7fde461b102b73b2cc5bb975587d59d4dd
Nurdin <hook4free@gmail.com>
2015-09-03 15:25:47 +0300

    Update calendar_lang.php

--- language/kyrgyz/calendar_lang.php
+++ language/kyrgyz/calendar_lang.php
@@ -1,1 +1,55 @@
-
+<?php
+/**
+ * System messages translation for CodeIgniter(tm)
+ *
+ * @author	CodeIgniter community / Immortality
+ * @copyright	Copyright (c) 2014 - 2015, British Columbia Institute of Technology (http://bcit.ca/)
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ * @link	http://codeigniter.com
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang['cal_su'] = 'Жк';
+$lang['cal_mo'] = 'Дш';
+$lang['cal_tu'] = 'Шш';
+$lang['cal_we'] = 'Шр';
+$lang['cal_th'] = 'Бш';
+$lang['cal_fr'] = 'Жм';
+$lang['cal_sa'] = 'Иш';
+$lang['cal_sun'] = 'Жк';
+$lang['cal_mon'] = 'Дш';
+$lang['cal_tue'] = 'Шш';
+$lang['cal_wed'] = 'Шр';
+$lang['cal_thu'] = 'Бш';
+$lang['cal_fri'] = 'Жм';
+$lang['cal_sat'] = 'Иш';
+$lang['cal_sunday'] = 'Жекшемби';
+$lang['cal_monday'] = 'Дүйшөнбү';
+$lang['cal_tuesday'] = 'Шейшемби';
+$lang['cal_wednesday'] = 'Шаршемби';
+$lang['cal_thursday'] = 'Бейшемби';
+$lang['cal_friday'] = 'Жума';
+$lang['cal_saturday'] = 'Ишемби';
+$lang['cal_jan'] = 'Үчт';
+$lang['cal_feb'] = 'Бир';
+$lang['cal_mar'] = 'Жал';
+$lang['cal_apr'] = 'Чын';
+$lang['cal_may'] = 'Буг';
+$lang['cal_jun'] = 'Кул';
+$lang['cal_jul'] = 'Тек';
+$lang['cal_aug'] = 'Баш';
+$lang['cal_sep'] = 'Аак';
+$lang['cal_oct'] = 'Тог';
+$lang['cal_nov'] = 'Жет';
+$lang['cal_dec'] = 'Беш';
+$lang['cal_january'] = 'Үчтүн айы';
+$lang['cal_february'] = 'Бирдин айы';
+$lang['cal_march'] = 'Жалган Куран';
+$lang['cal_april'] = 'Чын Куран';
+$lang['cal_mayl'] = 'Чын Куран';
+$lang['cal_june'] = 'Кулжа';
+$lang['cal_july'] = 'Теке';
+$lang['cal_august'] = 'Баш оона';
+$lang['cal_september'] = 'Аяк оона';
+$lang['cal_october'] = 'Тогуздун айы';
+$lang['cal_november'] = 'Жетинин айы';
+$lang['cal_december'] = 'Бештин айы';
